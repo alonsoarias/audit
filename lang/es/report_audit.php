172d908592d84cb28c5015f2de8dabcf6b01a09b
Alonso Arias <56093780+alonsoarias@users.noreply.github.com>
2024-09-21 11:06:39 -0500

    Create report_audit.php

---
+++ lang/es/report_audit.php
@@ -0,0 +1,28 @@
+<?php
+$string['pluginname'] = 'Reporte de Auditoría';
+$string['audit:view'] = 'Ver reporte de auditoría';
+$string['audit_report'] = 'Reporte de Auditoría';
+$string['fullname'] = 'Nombre Completo';
+$string['timestart'] = 'Hora de Inicio';
+$string['timeend'] = 'Hora de Fin';
+$string['filter'] = 'Filtro';
+$string['time'] = 'Hora';
+$string['event'] = 'Evento';
+$string['component'] = 'Componente';
+$string['action'] = 'Acción';
+$string['target'] = 'Objetivo';
+$string['objecttable'] = 'Tabla de Objetos';
+$string['objectid'] = 'ID del Objeto';
+$string['description'] = 'Descripción';
+$string['ip'] = 'Dirección IP';
+$string['all'] = 'Todos';
+$string['download_format'] = 'Formato de descarga';
+$string['download'] = 'Descargar';
+$string['error_loading_data'] = 'Error al cargar los datos de usuarios. Por favor, inténtelo de nuevo.';
+$string['error_updating_report'] = 'Error al actualizar el reporte. Por favor, inténtelo de nuevo.';
+$string['select_date'] = 'Seleccione fecha';
+$string['error_downloading'] = 'Error al descargar el reporte. Por favor, inténtelo de nuevo.';
+
+// Encabezados de la tabla de reportes de auditoría
+$string['total_records'] = 'Total de Registros';
+?>
